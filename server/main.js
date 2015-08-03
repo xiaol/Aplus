128fd4702251d86af2e709e8bb47e02242c05c66
Ivan liu <liu.xiao.in@gmail.com>
2015-08-03 13:07:01 +0800

    update html

--- server/main.js
+++ server/main.js
@@ -1,15 +1,14 @@
 // Uncomment the code below and fill in your information.
 // Then start Meteor in your project to create an initial admin user
 
-/*
+
 if (Meteor.users.find().count() === 0) {
   var userObj = {
-      username: 'username', 
-      email: 'username@gmail.com', 
-      password: 'username1234',
-      profile: {name: 'Your Name'},
+      username: 'xiaol', 
+      email: 'liuxiao.in@qq.com', 
+      password: 'qpalzm4',
+      profile: {name: 'xiaol'},
       roles: 'admin',
     };
   Accounts.createUser(userObj);
 }
-*/
