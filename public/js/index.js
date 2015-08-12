23ea9a1dbe167520b2d37716c33c945930809f8f
xiaotian93 <1172808428@qq.com>
2015-08-12 16:32:56 +0800

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,48 @@
 $(function(){
     var url=unescape(location.search);
-    $(window).ready(function(){if(url.indexOf("?")!=-1){var str=url.substr(url.indexOf("=")+1);}else{location.href="error.html";}$.ajax({url: "http://api.deeporiginalx.com/news/baijia/newsFetchContent", type: "post", async:false, data:{"url":str,}, datatype:"json",contentType: "application/x-www-form-urlencoded; charset=utf-8", success: function (data) {var cons=data['content'];var time=data['updateTime'];var abs=data['abs'];$(".date").html(time);$(".card1").html(abs);for(var i=0;i<cons.length;i++){for(var j in cons[i]){for(var k in cons[i][j]){if(k=="img"){$("#bannerImg").attr("src",cons[i][j][k]);}else if(k=="img_info"){$(".bannertitle").html(cons[i][j][k]);}else if(k=="txt"){$("<div class='card2'></div>").html(cons[i][j][k]).appendTo(".cardbox");}}}}},error: function () {alert("失败");}})})
+    $(window).ready(function(){
+        function GetRequest() {
+            var url =location.search; //获取url中"?"符后的字串
+            var theRequest = new Object();
+            if (url.indexOf("?") != -1) {
+                var str = url.substr(1);
+                strs = str.split("&");
+                for(var i = 0; i < strs.length; i ++) {
+                    theRequest[strs[i].split("=")[0]]=unescape(strs[i].split("=")[1]);
+                }return theRequest;
+            }else{
+                location.href="error.html";
+            }
+
+        }
+        var Request = new Object();
+        Request =GetRequest();
+        var str=Request['url'];
+        //var type=Request['type'];
+        //console.log(str);
+        //console.log(type);
+        $.ajax({
+            url: "http://api.deeporiginalx.com/news/baijia/newsFetchContent",
+            type: "post",
+            async:false,
+            data:{"url":str,},
+            datatype:"json",
+            contentType: "application/x-www-form-urlencoded; charset=utf-8",
+            success: function (data) {
+                var cons=data['content'];
+                var time=data['updateTime'];
+                var abs=data['abs'];$(".date").html(time);$(".card1").html(abs);
+                for(var i=0;i<cons.length;i++){
+                    for(var j in cons[i]){
+                        for(var k in cons[i][j]){
+                            if(k=="img"){$("#bannerImg").attr("src",cons[i][j][k]);
+                            }else if(k=="img_info"){
+                                $(".bannertitle").html(cons[i][j][k]);
+                            }else if(k=="txt"){
+                                //$("<div class='cardChild'></div>").html(j).appendTo(".card2");
+
+                                    $("<div class='card2'></div>").html(cons[i][j][k]).appendTo(".cardbox");}}}}},
+            error: function () {alert("失败");}})})
     $(".close").click(function(){$(".footer").css("display","none");})
     var u = navigator.userAgent, app = navigator.appVersion;
     var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //android终端或者uc浏览器
