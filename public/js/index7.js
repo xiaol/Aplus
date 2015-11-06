e80f8ef5c37b4886c1e3dbd36fb659df6bc44b5d
jianghao <jianghao@jianghaodeair.lan>
2015-11-06 16:41:43 +0800

    change jiekou

--- public/js/index7.js
+++ public/js/index7.js
@@ -24,6 +24,7 @@ $(function(){
     $(window).ready(function(){
         function GetRequest() {
             var url =location.search; //获取url中"?"符后的字串
+            //url = "http://deeporiginalx.com/news.html?type=1&newsid=3774fcbf55929622d58e5336c51e67ad&collection=NewsItem";
             if(url==''){
                 $(".bannerzhe").css("display","none");
             }
@@ -37,19 +38,17 @@ $(function(){
             }else{
                 location.href="error.html";
             }
-
         }
         var Request = new Object();
         Request =GetRequest();
         var str=Request['newsid'];
         var type=Request['type'];
-        var collection=Request['collection']
+        var collection=Request['collection'];
         if(type==1){
             $.ajax({
-                url: "121.40.34.56/news/baijia/fetchDetail",
+                url: "http://121.40.34.56/news/baijia/fetchDetail",
                 type: "post",
-                cache:"false",
-                async:"false",
+                async:false,
                 data:{"newsid":str,"collection":collection},
                 datatype:"json",
                 contentType: "application/x-www-form-urlencoded; charset=utf-8",
@@ -64,6 +63,7 @@ $(function(){
                     }else{
                         $("<div class='card1'></div>").html(abs).appendTo(".cardbox");
                     }
+
                     var img=data['imgUrl'];
                     if(img==null||""){
                         $("#bannerImg").attr("src","img/guanggao.png")
@@ -71,44 +71,31 @@ $(function(){
                         $("#bannerImg").attr("src",img);
                     }
                     var title=data['title'];
-                    if(title==null||""){
-                        return;
-                    }else{
-                        $(".bannertitle").html(title);
-                        document.title=title;
-                    }
-
-                    var arr = uniqeByKeys(point,['paragraphIndex']);
+                    $(".bannertitle").html(title);
+                    var arr = uniqeByKeys(point,['paragraphIndex']);console.log(arr);
                     for(var i=0;i<cons.length;i++){
                         for(var j in cons[i]){
-                            for(var k in cons[i][j]){
-                                if(k=="img"&&cons[i][j][k]!=img){
-                                    $("<div class='card2'></div>").attr({"src":cons[i][j][k],"id":"card"+j}).css({margin:"15px auto",padding:"0"}).appendTo(".cardbox");
-                                    $("<img class='card2'>").attr("src",cons[i][j][k]).css("padding","0").appendTo("#card"+j);
-                                }else{
-                                    $("<div class='card2'></div>").attr("id","card"+j).html(cons[i][j][k]).appendTo(".cardbox");
-                                }
-                            }}}
+                            if(j=="img"){
+                                $("<div class='card2'></div>").attr({"src":cons[i][j],"id":"card"+j}).css("padding","0px").appendTo(".cardbox");
+                                $("<img class='card2'>").attr("src",cons[i][j]).css("padding","0").appendTo("#card"+j);
+                            }else if(j=="txt"){
+                                $("<div class='card2'></div>").attr("id","card"+j).html(cons[i][j]).appendTo(".cardbox");
+                            }
+                        }}
                     for(var k=0;k<arr.length;k++) {
-                        $("<div class='cardChild'</div>").attr("id","child"+arr[k]['paragraphIndex']).appendTo("#card"+arr[k]['paragraphIndex']);
-                        $("<div class='pinglunword'></div>").appendTo("#child"+arr[k]['paragraphIndex']);
+                        $("<div class='cardChild'</div>").attr("id","child"+arr[k]['paragraphIndex']).appendTo("#card"+arr[k]['paragraphIndex']);//根据id，为相应的段落添加评论
+                        $("<div class='pinglunword'></div>").appendTo("#child"+arr[k]['paragraphIndex']);//添加评论，这里用k做测试，结果是第一段添加1，第二段添加0，1；想要结果是第一段添加1，第二段添加0，第二段不需要重复添加第一段已经加过的内容；
                         $("<div class='pinglunperson'></div>").appendTo("#child"+arr[k]['paragraphIndex']);
                         $("<img src="+arr[k]['userIcon']+">").css({"background-clip":"content-box",width:"100%"}).appendTo(".pinglunperson");
                         $("<div class='pinglunword'></div>").html(arr[k]['srcText']).appendTo("#child"+arr[k]['paragraphIndex']);
                         $("<div class='pinglunshu'></div>").html(arr[k]['comments_count']).appendTo("#child"+arr[k]['paragraphIndex']);
-                        $("<div class='pinglunmore'></div>").appendTo("#child"+arr[k]['paragraphIndex']);
-                        $("<img src='img/pingluntu.png' class='pinglundown'>").css("width","100%").appendTo(".pinglunmore");
+                        $("<div class='pinglunmore'></div>").css("background-clip","content-box").appendTo("#child"+arr[k]['paragraphIndex']);
                         $(".pinglunmore").click(function(){
                             $(".zhezhao").css({"display":"block","height":ch});
                         })
-
-
                     }
                 },
-                error: function () {
-                    alert("失败");
-                }
-            })
+                error: function () {alert("失败");}})
         }else if(type==0){
             $.ajax({
                 url:"http://api.deeporiginalx.com/news/baijia/fetchContent?url="+str,
