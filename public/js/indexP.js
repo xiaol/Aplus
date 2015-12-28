78857c9d00c1a92133d59edc96d8b9a7ec8e81b7
jianghao <jianghao@jianghaodeair.lan>
2015-12-28 15:44:24 +0800

    add more

--- public/js/indexP.js
+++ public/js/indexP.js
@@ -12,6 +12,19 @@ $(function(){
             $(this).find(".sanjiao").animate({"opacity":"0"});
         })
     })
+    $(".navBoxB").find("a").each(function(i,index) {
+        $(this).hover(function () {
+            $(".underline").css("opacity","0");
+            $(".sanjiao").css("opacity","0");
+            $(this).find(".underline").animate({"opacity":"1"});
+            $(this).find(".sanjiao").animate({"opacity":"1"});
+        }, function () {
+            $(this).find(".underline").stop();
+            $(this).find(".sanjiao").stop();
+            $(this).find(".underline").animate({"opacity":"0"});
+            $(this).find(".sanjiao").animate({"opacity":"0"});
+        })
+    })
     $('.tiyanBox').each(function(i,index){
         $(this).hover(function(){
             $(this).find(".tiyan").css("color","#6cc1fb");
