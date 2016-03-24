6977682a0bb9db9035145aca864ff3b0e5e9eb20
Gavin <Gavin@WeiliangGuosMac.lan>
2016-03-24 11:08:05 +0800

    new code

---
+++ public/branch/js/news.js
@@ -0,0 +1,7 @@
+$(function() {
+	var p = news.compute('蔡依林做手表蛋糕让妈妈');
+    var div = document.getElementById("test");
+    // alert(($("#test").width()));
+    // console.log($("#test").height());
+    // alert(p.w + '\n' + p.h);
+});
